d326804fe7a4a3ac70a73130d229c0626834731b
muhammedtalhasaglam <talha.saglam@tokeninc.com>
2022-05-04 15:47:10 +0300

    Initial Commit

---
+++ Crypto App Clone/Crypto_App_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_App_CloneApp.swift
+//  Crypto App Clone
+//
+//  Created by Muhammed Talha Sağlam on 4.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_App_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
