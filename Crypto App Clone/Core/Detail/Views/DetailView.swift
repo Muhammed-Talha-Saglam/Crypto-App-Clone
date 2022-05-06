80603be40900adaad1cbcdaac6d0a79f490ab0b3
muhammedtalhasaglam <talha.saglam@tokeninc.com>
2022-05-06 15:14:10 +0300

    Feature: Added line chart view.

--- Crypto App Clone/Core/Detail/Views/DetailView.swift
+++ Crypto App Clone/Core/Detail/Views/DetailView.swift
@@ -34,21 +34,27 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
-
-                overViewTitle
-                Divider()
-                overViewGrid
-                additionalTitle
-                Divider()
-                additionalViewGrid
-                
+            VStack {
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
+                VStack(spacing: 20) {
+                    overViewTitle
+                    Divider()
+                    overViewGrid
+                    additionalTitle
+                    Divider()
+                    additionalViewGrid
+                    
+                }
+                .padding()
             }
-            .padding()
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -61,6 +67,17 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
     private var overViewTitle: some View {
         Text("Overview")
             .font(.title)
